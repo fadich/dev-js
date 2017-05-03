18de6b449045b8f403300c87ba39cc8e9c41dd12
fadich <fadi.ahmad@wdg.com.ua>
2017-05-03 14:17:01 +0300

    ``` issue #11 > restructure > environment

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,11 @@
 let express = require('express');
 let app = express();
 
+import {mainPanel} from "./templates/index";
+
 app.get('/', function (req, res) {
     res.send(
-        `
-        <!doctype html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport"
-                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
-        </head>
-        <body>
-        </body>
-        </html>
-        `
+        mainPanel
     );
 });
 
---
+++ src/server/templates/index.js
@@ -0,0 +1,5 @@
+let mainPanel = `
+<div>Some</div>
+`;
+
+export {mainPanel};
