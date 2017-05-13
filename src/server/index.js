2af914435fd0ed1702b2be22af5a2129e1be5c23
fadich <fadi.ahmad@wdg.com.ua>
2017-05-13 15:52:20 +0300

    ``` issue #11 > server restructure > socket

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,29 @@
-let express = require('express');
-let app = express();
-
+import express from "express";
+import io from 'socket.io';
 import {index} from "./templates/panel/panel";
+
+let app = express();
+let socket = io();
+
+socket.on('connection', function(socket) {
+    socket.on('test', function() {
+        socket.emit('require', { type: "aaa", link: "bbb" });
+    });
+});
 
 app.get('/', function (req, res) {
     res.send(index);
+});
+
+app.get('/require/{module}', function (req, res) {
+    console.log(123);
+    switch (req.param('module')) {
+        case 'socket':
+            res.sendFile(io);
+            break;
+        default:
+            console.log("Trying to require unsupported module.");
+    }
 });
 
 app.listen(4242, function () {
--- src/server/templates/panel/panel.js
+++ src/server/templates/panel/panel.js
@@ -1,5 +1,22 @@
 let index = `
-<div>Some</div>
+<div class="royal-mainPanel  + (new HtmlDocument()).identityClass + ">
+    <div class="royal-panel-nav">
+        <button class="royal-close-panel royal-button"
+            onclick="(new HtmlDocument())
+                .clear(document.getElementsByClassName(
+                    (new HtmlDocument()).identityClass)
+                )">
+                <i class="material-icons">settings_power</i> +
+        </button>
+        <div class="royal-dragButton"><i class="material-icons">open_with</i></div>
+    </div>
+    <hr>
+    <div class="royal-modules royal-dev-js">
+        Loading. . .
+    </div>
+</div>
+
+<script src="/require/socket" class="royal-dev-js"></script>
 `;
 
 export {index};
